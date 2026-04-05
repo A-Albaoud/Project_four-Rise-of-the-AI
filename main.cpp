f24bed112f0af85b61ee71d909f61641e1376b89
A-Albaoud <abdulrahman.thebaoud@gmail.com>
2026-04-04 23:23:29 -0400

    pledge

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/**
+* Author: Abdulrahman Albaoud
+* Assignment: Rise of the AI
+* Date due: 2025-11-08, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 #include "CS3113/LevelB.h"
 #include "CS3113/Menu.h"
 #include "CS3113/Gameover.h"
